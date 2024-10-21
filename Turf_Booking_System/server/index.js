13585a16ab8d902b3e479ed8bfb6b0b006fc85b4
karn pable <karnpable5777@gmail.com>
2024-10-21 21:26:23 +0530

    .

--- Turf_Booking_System/server/index.js
+++ Turf_Booking_System/server/index.js
@@ -29,6 +29,7 @@ app.use(cors());
 app.use(
     fileUpload({
       useTempFiles: true,
+      tempFileDir: "/tmp",
     })
   );
   
--- Turf_Booking_System/server/utils/mailSender.js
+++ Turf_Booking_System/server/utils/mailSender.js
@@ -1,5 +1,3 @@
-// const nodeMailer = require("nodemailer");
-
 
 const nodemailer = require("nodemailer");
 require('dotenv').config()
@@ -11,8 +9,8 @@ const mailSender = async (email, title, body) => {
                 host: 'smtp.gmail.com',
                 port: 587,
                 auth:{
-                    user: 'karnpable5777@gmail.com',
-                    pass: 'zizz lzqf nedv grfq'
+                    user:  process.env.MAIL_USER,
+                    pass:  process.env.MAIL_PASS
                 }
             })
 
